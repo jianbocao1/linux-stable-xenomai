408de7d7c7764f5ea1c58d188c08e37314746ada
Kareem Shaik <kareem.m.shaik@intel.com>
2017-10-31 15:44:52 +0530

    ASoC: Intel: Skylake: Return default sampling rate for Probe compress devices.
    Defining sampling rate for getting Probe data from probe compress
    devices. As user space expects a valid sample rate but existing API
    compress_get_hpointer returns an error if sampling rate is 0.
    
    Change-Id: I0482c96fea4583a93b952d7484a4234388d6a6f0
    Signed-off-by: Kareem Shaik <kareem.m.shaik@intel.com>
    Reviewed-on:
    Reviewed-by: B, Jayachandran <jayachandran.b@intel.com>
    Reviewed-by: Kesapragada, Pardha Saradhi <pardha.saradhi.kesapragada@intel.com>
    Reviewed-by: Tewani, Pradeep D <pradeep.d.tewani@intel.com>
    Reviewed-by: Koul, Vinod <vinod.koul@intel.com>
    Tested-by: Sm, Bhadur A <bhadur.a.sm@intel.com>

--- sound/soc/intel/skylake/skl-pcm.c
+++ sound/soc/intel/skylake/skl-pcm.c
@@ -1421,6 +1421,9 @@ static struct snd_soc_dai_driver skl_platform_dai[] = {
 	.playback = {
 		.stream_name = "Probe Playback",
 		.channels_min = HDA_MONO,
+		.rates = SNDRV_PCM_RATE_48000,
+		.rate_min = 48000,
+		.rate_max = 48000,
 	},
 },
 {
@@ -1430,6 +1433,9 @@ static struct snd_soc_dai_driver skl_platform_dai[] = {
 	.capture = {
 			.stream_name = "Probe Capture",
 			.channels_min = HDA_MONO,
+			.rates = SNDRV_PCM_RATE_48000,
+			.rate_min = 48000,
+			.rate_max = 48000,
 	},
 },
 };
