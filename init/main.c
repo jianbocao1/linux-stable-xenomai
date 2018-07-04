ac8d86799ce9fb52ebabc9db0f0fd2bbed69ea06
Philippe Gerum <rpm@xenomai.org>
2018-07-04 11:50:21 +0200

    genirq: add generic I-pipe core
    This commit provides the arch-independent bits for implementing the
    interrupt pipeline core, a lightweight layer introducing a separate,
    high-priority execution stage for handling all IRQs in pseudo-NMI
    mode, which cannot be delayed by the regular kernel code. See
    Documentation/ipipe.rst for details about interrupt pipelining.
    
    Architectures which support interrupt pipelining should select
    HAVE_IPIPE_SUPPORT, along with implementing the required arch-specific
    code. In such a case, CONFIG_IPIPE becomes available to the user via
    the Kconfig interface for enabling the feature.

--- init/main.c
+++ init/main.c
@@ -46,6 +46,7 @@
 #include <linux/cpuset.h>
 #include <linux/cgroup.h>
 #include <linux/efi.h>
+#include <linux/ipipe.h>
 #include <linux/tick.h>
 #include <linux/sched/isolation.h>
 #include <linux/interrupt.h>
@@ -584,7 +585,7 @@ asmlinkage __visible void __init start_kernel(void)
 
 	cgroup_init_early();
 
-	local_irq_disable();
+	hard_local_irq_disable();
 	early_boot_irqs_disabled = true;
 
 	/*
@@ -624,6 +625,7 @@ asmlinkage __visible void __init start_kernel(void)
 	setup_log_buf(0);
 	vfs_caches_init_early();
 	sort_main_extable();
+	__ipipe_init_early();
 	trap_init();
 	mm_init();
 
@@ -694,6 +696,11 @@ asmlinkage __visible void __init start_kernel(void)
 	boot_init_stack_canary();
 
 	time_init();
+	/*
+	* We need to wait for the interrupt and time subsystems to be
+	* initialized before enabling the pipeline.
+	*/
+	__ipipe_init();
 	perf_event_init();
 	profile_init();
 	call_function_init();
@@ -1028,6 +1035,7 @@ static void __init do_basic_setup(void)
 	cpuset_init_smp();
 	driver_init();
 	init_irq_proc();
+	__ipipe_init_proc();
 	do_ctors();
 	usermodehelper_enable();
 	do_initcalls();
