tingai1/device-androidia-kernel

Soundwire squashed commits
SoundWire: Add place holder for the soundwire bus driver header file

This patch creates the place holder file for the SoundWire
bus driver header file.

Change-Id: Ib71bbe10f5988194ee24f3793950a6c079d56aa7
Signed-off-by: Hardik T Shah <hardik.t.shah@intel.com>

SoundWire: Add #defines to the bus driver

This patch adds the #defines to the SoundWire bus
driver.

Change-Id: Ic4dfa9582aa45c3685aee021c156cfbc3e23d817
Signed-off-by: Hardik T Shah <hardik.t.shah@intel.com>

SoundWire: Add enum to the SoundWire bus driver

This patch adds the enums for the SoundWire bus driver
header file.

Change-Id: Id9b48b5bacc9137f62099eaa4948390b54d4aac0
Signed-off-by: Hardik T Shah <hardik.t.shah@intel.com>

SoundWire: Add forward declaration for the data structures.

This patch adds the forward declarations of data
structures for the SoundWire bus driver.

Change-Id: I8d5d8dc885c7c8a28d68bada9d4b270c603ae4e4
Signed-off-by: Hardik T Shah <hardik.t.shah@intel.com>

SoundWire: Add device_id table for SoundWire bus.

Add device_id table for the soundwire master and slave
devices and driver registration

Change-Id: I3848c9476459215ff1d301af69e2eb3c8934bb60
Signed-off-by: Hardik T Shah <hardik.t.shah@intel.com>

SoundWire: Add data structures for slave driver and device.

This patch adds the data structure for slave driver
and device registration with SoundWire bus driver.

Change-Id: Ie7f2a0a39fcde3073b560fde41f6c91684975cbe
Signed-off-by: Hardik T Shah <hardik.t.shah@intel.com>

SoundWire: Add data structures for master driver and device.

This patch adds the data structure for master driver
and device registration with SoundWire bus driver.

Change-Id: I269c3afa0e94979274d0415cfbd0be4c214d1afb
Signed-off-by: Hardik T Shah <hardik.t.shah@intel.com>

SoundWire: Add message transfer data structure

This patch adds the data structure to do register read/write
using message tranasfer. This is similar to i2c_transfer
on i2c bus.

Change-Id: Id10bd88b4fc02c22833347

drivers/sdw/sdw_maxim.c
@@ -1,0 +1,146 @@
+/*
+ * sdw_maxim.c -- Maxim SoundWire slave device driver. Dummy driver
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ */
+
+#include <linux/delay.h>
+#include <linux/sdw_bus.h>
+#include <linux/module.h>
+
+
+static int maxim_register_sdw_capabilties(struct sdw_slave *sdw,
+				 const struct sdw_slave_id *sdw_id)
+{
+	struct sdw_slv_capabilities cap;
+	struct sdw_slv_dpn_capabilities *dpn_cap = NULL;
+	struct port_audio_mode_properties *prop = NULL;
+	int i, j;
+
+	cap.wake_up_unavailable = true;
+	cap.test_mode_supported = false;
+	cap.clock_stop1_mode_supported = false;
+	cap.simplified_clock_stop_prepare = false;
+	cap.highphy_capable = true;
+	cap.paging_supported = false;
+	cap.bank_delay_support = false;
+	cap.port_15_read_behavior = 0;
+	cap.sdw_dp0_supported = false;
+	cap.num_of_sdw_ports = 3;
+	cap.sdw_dpn_cap = devm_kzalloc(&sdw->dev,
+			((sizeof(struct sdw_slv_dpn_capabilities)) *
+			cap.num_of_sdw_ports), GFP_KERNEL);
+	for (i = 0; i < cap.num_of_sdw_ports; i++) {
+		dpn_cap = &cap.sdw_dpn_cap[i];
+		if (i == 0 || i == 2)
+			dpn_cap->port_direction = SDW_PORT_SOURCE;
+		else
+			dpn_cap->port_direction = SDW_PORT_SINK;
+
+		dpn_cap->port_number = i+1;
+		dpn_cap->max_word_length =  24;
+		dpn_cap->min_word_length = 16;
+		dpn_cap->num_word_length = 0;
+		dpn_cap->word_length_buffer = NULL;
+		dpn_cap->dpn_type = SDW_FULL_DP;
+		dpn_cap->dpn_grouping = SDW_BLOCKGROUPCOUNT_1;
+		dpn_cap->prepare_ch = SDW_CP_SM;
+		dpn_cap->imp_def_intr_mask = 0x0;
+		dpn_cap->min_ch_num = 1;
+		dpn_cap->max_ch_num = 2;
+		dpn_cap->num_ch_supported = 0;
+		dpn_cap->ch_supported = NULL;
+		dpn_cap->port_flow_mode_mask = SDW_PORT_FLOW_MODE_ISOCHRONOUS;
+		dpn_cap->block_packing_mode_mask =
+				SDW_PORT_BLK_PKG_MODE_BLK_PER_PORT_MASK |
+				SDW_PORT_BLK_PKG_MODE_BLK_PER_CH_MASK;
+		dpn_cap->port_encoding_type_mask =
+				SDW_PORT_ENCODING_TYPE_TWOS_CMPLMNT |
+				SDW_PORT_ENCODING_TYPE_SIGN_MAGNITUDE |
+				SDW_PORT_ENCODING_TYPE_IEEE_32_FLOAT;
+		dpn_cap->num_audio_modes = 1;
+
+		dpn_cap->mode_properties = devm_kzalloc(&sdw->dev,
+				((sizeof(struct port_audio_mode_properties)) *
+				dpn_cap->num_audio_modes), GFP_KERNEL);
+		for (j = 0; j < dpn_cap->num_audio_modes; j++) {
+			prop = &dpn_cap->mode_properties[j];
+			prop->max_frequency = 16000000;
+			prop->min_frequency = 1000000;
+			prop->num_freq_configs = 0;
+			prop->freq_supported = NULL;
+			prop->glitchless_transitions_mask = 0x1;
+			prop->max_sampling_frequency = 192000;
+			prop->min_sampling_frequency = 8000;
+			prop->num_sampling_freq_configs = 0;
+			prop->sampling_freq_config = NULL;
+			prop->ch_prepare_behavior = SDW_CH_PREP_ANY_TIME;
+		}
+	}
+	return sdw_register_slave_capabilities(sdw, &cap);
+
+}
+static int maxim_sdw_probe(struct sdw_slave *sdw,
+				 const struct sdw_slave_id *sdw_id)
+{
+	dev_info(&sdw->dev, "Maxim SoundWire Slave Registered %lx\n", sdw_id->driver_data);
+	return maxim_register_sdw_capabilties(sdw, sdw_id);
+}
+
+static int maxim_sdw_remove(struct sdw_slave *sdw)
+{
+	dev_info(&sdw->dev, "Maxim SoundWire Slave un-Registered\n");
+	return 0;
+}
+
+static const struct sdw_slave_id maxim_id[] = {
+	{"03:01:9f:79:00:00", 0},
+	{"09:01:9f:79:00:00", 1},
+	{"04:01:9f:79:00:00", 2},
+	{"0a:01:9f:79:00:00", 3},
+	{"04:01:9f:79:00:00", 4},
+	{"0a:01:9f:79:00:00", 5},
+	{"05:01:9f:79:00:00", 6},
+	{"06:01:9f:79:00:00", 7},
+	{"05:01:9f:79:00:00", 8},
+	{"00:01:9f:79:00:00", 9},
+	{"06:01:9f:79:00:00", 10},
+	{"07:01:9f:79:00:00", 11},
+	{"00:01:9f:79:00:00", 12},
+	{"06:01:9f:79:00:00", 13},
+	{"01:01:9f:79:00:00", 14},
+	{"07:01:9f:79:00:00", 15},
+	{"08:01:9f:79:00:00", 16},
+	{"01:01:9f:79:00:00", 17},
+	{"07:01:9f:79:00:00", 18},
+	{"02:01:9f:79:00:00", 19},
+	{"08:01:9f:79:00:00", 20},
+	{"09:01:9f:79:00:00", 21},
+	{"02:01:9f:79:00:00", 22},
+	{"08:01:9f:79:00:00", 23},
+	{"03:01:9f:79:00:00", 24},
+	{"09:01:9f:79:00:00", 25},
+	{"0a:01:9f:79:00:00", 26},
+	{},
+};
+
+MODULE_DEVICE_TABLE(sdw, maxim_id);
+
+static struct sdw_slave_driver maxim_sdw_driver = {
+	.driver_type = SDW_DRIVER_TYPE_SLAVE,
+	.driver = {
+		   .name = "maxim",
+		   },
+	.probe = maxim_sdw_probe,
+	.remove = maxim_sdw_remove,
+	.id_table = maxim_id,
+};
+
+module_sdw_slave_driver(maxim_sdw_driver);
+
+MODULE_DESCRIPTION("SoundWire Maxim Slave Driver");
+MODULE_AUTHOR("Hardik Shah, <hardik.t.shah@intel.com>");
+MODULE_LICENSE("GPL");

drivers/sdw/sdw_priv.h
@@ -1,0 +1,243 @@
+/*
+ *  sdw_priv.h - Private definition for sdw bus interface.
+ *
+ *  Copyright (C) 2014-2015 Intel Corp
+ *  Author:  Hardik Shah  <hardik.t.shah@intel.com>
+ *
+ *  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
+ */
+
+#ifndef _LINUX_SDW_PRIV_H
+#define _LINUX_SDW_PRIV_H
+
+#include <linux/kthread.h>	/* For kthread */
+#include <linux/spinlock.h>
+
+#define SDW_MAX_STREAM_TAG_KEY_SIZE	80
+#define SDW_NUM_STREAM_TAGS		100
+#define MAX_NUM_ROWS			23 /* As per SDW Spec */
+#define MAX_NUM_COLS			8/* As per SDW Spec */
+#define MAX_NUM_ROW_COLS		(MAX_NUM_ROWS * MAX_NUM_COLS)
+
+#define SDW_STATE_INIT_STREAM_TAG	    0x1
+#define SDW_STATE_ALLOC_STREAM              0x2
+#define SDW_STATE_CONFIG_STREAM             0x3
+#define SDW_STATE_COMPUTE_STREAM	    0x4
+#define SDW_STATE_PREPARE_STREAM            0x5
+#define SDW_STATE_ENABLE_STREAM             0x6
+#define SDW_STATE_DISABLE_STREAM            0x7
+#define SDW_STATE_UNPREPARE_STREAM          0x8
+#define SDW_STATE_UNCOMPUTE_STREAM	    0x9
+#define SDW_STATE_RELEASE_STREAM            0xa
+#define SDW_STATE_FREE_STREAM               0xb
+#define SDW_STATE_FREE_STREAM_TAG           0xc
+#define SDW_STATE_ONLY_XPORT_STREAM	    0xd
+
+#define SDW_STATE_INIT_RT		0x1
+#define SDW_STATE_CONFIG_RT		0x2
+#define SDW_STATE_PREPARE_RT		0x3
+#define SDW_STATE_ENABLE_RT		0x4
+#define SDW_STATE_DISABLE_RT		0x5
+#define SDW_STATE_UNPREPARE_RT		0x6
+#define SDW_STATE_RELEASE_RT		0x7
+
+struct sdw_runtime;
+/* Defined in sdw.c, used by multiple files of module */
+extern struct sdw_core sdw_core;
+
+enum sdw_port_state {
+	SDW_PORT_STATE_CH_READY,
+	SDW_PORT_STATE_CH_STOPPED,
+	SDW_PORT_STATE_CH_PREPARING,
+	SDW_PORT_STATE_CH_DEPREPARING,
+};
+
+enum sdw_stream_state {
+	SDW_STREAM_ALLOCATED,
+	SDW_STREAM_FREE,
+	SDW_STREAM_ACTIVE,
+	SDW_STREAM_INACTIVE,
+};
+
+enum sdw_clk_state {
+	SDW_CLK_STATE_OFF = 0,
+	SDW_CLK_STATE_ON = 1,
+};
+
+struct port_chn_en_state {
+	bool is_activate;
+	bool is_bank_sw;
+};
+
+struct sdw_stream_tag {
+	int stream_tag;
+	struct mutex stream_lock;
+	int ref_count;
+	enum sdw_stream_state stream_state;
+	char key[SDW_MAX_STREAM_TAG_KEY_SIZE];
+	struct sdw_runtime *sdw_rt;
+};
+
+struct sdw_stream_params {
+	unsigned int rate;
+	unsigned int channel_count;
+	unsigned int bps;
+};
+
+struct sdw_port_runtime {
+	int port_num;
+	enum sdw_port_state port_state;
+	int channel_mask;
+	/* Frame params and stream params are per port based
+	 * Single stream of audio may be split
+	 * into mutliple port each handling
+	 * subset of channels, channels should
+	 * be contiguous in subset
+	 */
+	struct sdw_transport_params transport_params;
+	struct sdw_port_params port_params;
+	struct list_head port_node;
+};
+
+struct sdw_slave_runtime {
+	/* Simplified port or full port, there cannot be both types of
+	 * data port for single stream, so data structure is kept per
+	 * slave runtime, not per port
+	 */
+	enum sdw_dpn_type type;
+	struct sdw_slave *slave;
+	int direction;
+	/* Stream may be split into multiple slaves, so this is for
+	 * this particular slave
+	 */
+	struct sdw_stream_params stream_params;
+	struct list_head port_rt_list;
+	struct list_head slave_sdw_node;
+	struct list_head slave_node;
+	int rt_state; /* State of runtime structure */
+
+};
+
+
+struct sdw_mstr_runtime {
+	struct sdw_master *mstr;
+	int direction;
+	/* Stream may be split between  multiple masters so this
+	 * is for invidual master, if stream is split into multiple
+	 * streams. For calculating the bandwidth on the particular bus
+	 * stream params of master is taken into account.
+	 */
+	struct sdw_stream_params stream_params;
+	struct list_head port_rt_list;
+	/* Two nodes are required because BW calculation is based on master
+	 * while stream enabling is based on stream_tag, where multiple
+	 * masters may be involved
+	 */
+	struct list_head mstr_sdw_node; /* This is to add mstr_rt in sdw_rt */
+	struct list_head mstr_node; /* This is to add mstr_rt in mstr */
+
+	struct list_head slv_rt_list;
+	 /* Individual stream bandwidth on given master */
+	unsigned int	stream_bw;
+	 /* State of runtime structure */
+	int rt_state;
+};
+
+struct sdw_runtime {
+	int tx_ref_count;
+	int rx_ref_count;
+	/* This is stream params for whole stream
+	 * but stream may be split between two
+	 * masters, or two slaves.
+	 */
+	struct sdw_stream_params stream_params;
+	struct list_head slv_rt_list;
+	struct list_head mstr_rt_list;
+	enum sdw_stream_type	type;
+	int stream_state;
+	int xport_state;
+
+};
+
+struct sdw_slv_status {
+	struct list_head node;
+	enum sdw_slave_status status[SOUNDWIRE_MAX_DEVICES];
+};
+
+/** Bus structure which handles bus related information */
+struct sdw_bus {
+	struct list_head bus_node;
+	struct sdw_master *mstr;
+	unsigned int	port_grp_mask[2];
+	unsigned int	slave_grp_mask[2];
+	unsigned int	clk_state;
+	unsigned int	active_bank;
+	unsigned int	clk_freq;
+	/* Bus total Bandwidth. Initialize and reset to zero */
+	unsigned int	bandwidth;
+	unsigned int	system_interval; /* Bus System Interval */
+	unsigned int	frame_freq;
+	unsigned int	col;
+	unsigned int	row;
+	struct task_struct *status_thread;
+	struct kthread_worker kworker;
+	struct kthread_work kwork;
+	struct list_head status_list;
+	spinlock_t spinlock;
+};
+
+/** Holds supported Row-Column combination related information */
+struct sdw_rowcol {
+	int     row;
+	int     col;
+	int     control_bits;
+	int data_bits;
+};
+
+/**
+ * Global soundwire structure. It handles all the streams spawned
+ * across masters and has list of bus structure per every master
+ * registered
+ */
+struct sdw_core {
+	struct sdw_stream_tag stream_tags[SDW_NUM_STREAM_TAGS];
+	struct sdw_rowcol     rowcolcomb[MAX_NUM_ROW_COLS];
+	struct list_head bus_list;
+	struct mutex core_lock;
+	struct idr idr;
+	int first_dynamic_bus_num;
+};
+
+/* Structure holding mapping of numbers to cols */
+struct sdw_num_to_col {
+	int num;
+	int col;
+};
+
+/* Structure holding mapping of numbers to rows */
+struct sdw_num_to_row {
+	int num;
+	int row;
+};
+
+int sdw_slave_port_config_port_params(struct sdw_slave_runtime *slv_rt);
+int sdw_slave_port_prepare(struct sdw_slave_runtime, bool prepare);
+int sdw_bus_bw_init(void);
+int sdw_mstr_bw_init(struct sdw_bus *sdw_bs);
+int sdw_bus_calc_bw(struct sdw_stream_tag *stream_tag, bool enable);
+int sdw_bus_calc_bw_dis(struct sdw_stream_tag *stream_tag, bool unprepare);
+int sdw_chn_enable(void);
+
+#endif /* _LINUX_SDW_PRIV_H */

jianbocao1/linux-stable-xenomai

drivers/sdw/sdw_maxim.c
@@ -1,0 +1,146 @@
+/*
+ * sdw_maxim.c -- Maxim SoundWire slave device driver. Dummy driver
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ */
+
+#include <linux/delay.h>
+#include <linux/sdw_bus.h>
+#include <linux/module.h>
+
+
+static int maxim_register_sdw_capabilties(struct sdw_slv *sdw,
+				 const struct sdw_slv_id *sdw_id)
+{
+	struct sdw_slv_capabilities cap;
+	struct sdw_slv_dpn_capabilities *dpn_cap = NULL;
+	struct port_audio_mode_properties *prop = NULL;
+	int i, j;
+
+	cap.wake_up_unavailable = true;
+	cap.test_mode_supported = false;
+	cap.clock_stop1_mode_supported = false;
+	cap.simplified_clock_stop_prepare = false;
+	cap.highphy_capable = true;
+	cap.paging_supported = false;
+	cap.bank_delay_support = false;
+	cap.port_15_read_behavior = 0;
+	cap.sdw_dp0_supported = false;
+	cap.num_of_sdw_ports = 3;
+	cap.sdw_dpn_cap = devm_kzalloc(&sdw->dev,
+			((sizeof(struct sdw_slv_dpn_capabilities)) *
+			cap.num_of_sdw_ports), GFP_KERNEL);
+	for (i = 0; i < cap.num_of_sdw_ports; i++) {
+		dpn_cap = &cap.sdw_dpn_cap[i];
+		if (i == 0 || i == 2)
+			dpn_cap->port_direction = SDW_PORT_SOURCE;
+		else
+			dpn_cap->port_direction = SDW_PORT_SINK;
+
+		dpn_cap->port_number = i+1;
+		dpn_cap->max_word_length =  24;
+		dpn_cap->min_word_length = 16;
+		dpn_cap->num_word_length = 0;
+		dpn_cap->word_length_buffer = NULL;
+		dpn_cap->dpn_type = SDW_FULL_DP;
+		dpn_cap->dpn_grouping = SDW_BLOCKGROUPCOUNT_1;
+		dpn_cap->prepare_ch = SDW_CP_SM;
+		dpn_cap->imp_def_intr_mask = 0x0;
+		dpn_cap->min_ch_num = 1;
+		dpn_cap->max_ch_num = 2;
+		dpn_cap->num_ch_supported = 0;
+		dpn_cap->ch_supported = NULL;
+		dpn_cap->port_flow_mode_mask = SDW_PORT_FLOW_MODE_ISOCHRONOUS;
+		dpn_cap->block_packing_mode_mask =
+				SDW_PORT_BLK_PKG_MODE_BLK_PER_PORT_MASK |
+				SDW_PORT_BLK_PKG_MODE_BLK_PER_CH_MASK;
+		dpn_cap->port_encoding_type_mask =
+				SDW_PORT_ENCODING_TYPE_TWOS_CMPLMNT |
+				SDW_PORT_ENCODING_TYPE_SIGN_MAGNITUDE |
+				SDW_PORT_ENCODING_TYPE_IEEE_32_FLOAT;
+		dpn_cap->num_audio_modes = 1;
+
+		dpn_cap->mode_properties = devm_kzalloc(&sdw->dev,
+				((sizeof(struct port_audio_mode_properties)) *
+				dpn_cap->num_audio_modes), GFP_KERNEL);
+		for (j = 0; j < dpn_cap->num_audio_modes; j++) {
+			prop = &dpn_cap->mode_properties[j];
+			prop->max_frequency = 16000000;
+			prop->min_frequency = 1000000;
+			prop->num_freq_configs = 0;
+			prop->freq_supported = NULL;
+			prop->glitchless_transitions_mask = 0x1;
+			prop->max_sampling_frequency = 192000;
+			prop->min_sampling_frequency = 8000;
+			prop->num_sampling_freq_configs = 0;
+			prop->sampling_freq_config = NULL;
+			prop->ch_prepare_behavior = SDW_CH_PREP_ANY_TIME;
+		}
+	}
+	return sdw_register_slave_capabilities(sdw, &cap);
+
+}
+static int maxim_sdw_probe(struct sdw_slv *sdw,
+				 const struct sdw_slv_id *sdw_id)
+{
+	dev_info(&sdw->dev, "Maxim SoundWire Slave Registered %lx\n", sdw_id->driver_data);
+	return maxim_register_sdw_capabilties(sdw, sdw_id);
+}
+
+static int maxim_sdw_remove(struct sdw_slv *sdw)
+{
+	dev_info(&sdw->dev, "Maxim SoundWire Slave un-Registered\n");
+	return 0;
+}
+
+static const struct sdw_slv_id maxim_id[] = {
+	{"03:01:9f:79:00:00", 0},
+	{"09:01:9f:79:00:00", 1},
+	{"04:01:9f:79:00:00", 2},
+	{"0a:01:9f:79:00:00", 3},
+	{"04:01:9f:79:00:00", 4},
+	{"0a:01:9f:79:00:00", 5},
+	{"05:01:9f:79:00:00", 6},
+	{"06:01:9f:79:00:00", 7},
+	{"05:01:9f:79:00:00", 8},
+	{"00:01:9f:79:00:00", 9},
+	{"06:01:9f:79:00:00", 10},
+	{"07:01:9f:79:00:00", 11},
+	{"00:01:9f:79:00:00", 12},
+	{"06:01:9f:79:00:00", 13},
+	{"01:01:9f:79:00:00", 14},
+	{"07:01:9f:79:00:00", 15},
+	{"08:01:9f:79:00:00", 16},
+	{"01:01:9f:79:00:00", 17},
+	{"07:01:9f:79:00:00", 18},
+	{"02:01:9f:79:00:00", 19},
+	{"08:01:9f:79:00:00", 20},
+	{"09:01:9f:79:00:00", 21},
+	{"02:01:9f:79:00:00", 22},
+	{"08:01:9f:79:00:00", 23},
+	{"03:01:9f:79:00:00", 24},
+	{"09:01:9f:79:00:00", 25},
+	{"0a:01:9f:79:00:00", 26},
+	{},
+};
+
+MODULE_DEVICE_TABLE(sdwint, maxim_id);
+
+static struct sdw_slave_driver maxim_sdw_driver = {
+	.driver_type = SDW_DRIVER_TYPE_SLAVE,
+	.driver = {
+		   .name = "maxim",
+		   },
+	.probe = maxim_sdw_probe,
+	.remove = maxim_sdw_remove,
+	.id_table = maxim_id,
+};
+
+module_sdw_slave_driver(maxim_sdw_driver);
+
+MODULE_DESCRIPTION("SoundWire Maxim Slave Driver");
+MODULE_AUTHOR("Hardik Shah, <hardik.t.shah@intel.com>");
+MODULE_LICENSE("GPL");

drivers/sdw/sdw_priv.h
@@ -1,0 +1,243 @@
+/*
+ *  sdw_priv.h - Private definition for sdw bus interface.
+ *
+ *  Copyright (C) 2014-2015 Intel Corp
+ *  Author:  Hardik Shah  <hardik.t.shah@intel.com>
+ *
+ *  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; version 2 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
+ */
+
+#ifndef _LINUX_SDW_PRIV_H
+#define _LINUX_SDW_PRIV_H
+
+#include <linux/kthread.h>	/* For kthread */
+#include <linux/spinlock.h>
+
+#define SDW_MAX_STREAM_TAG_KEY_SIZE	80
+#define SDW_NUM_STREAM_TAGS		100
+#define MAX_NUM_ROWS			23 /* As per SDW Spec */
+#define MAX_NUM_COLS			8/* As per SDW Spec */
+#define MAX_NUM_ROW_COLS		(MAX_NUM_ROWS * MAX_NUM_COLS)
+
+#define SDW_STATE_INIT_STREAM_TAG	    0x1
+#define SDW_STATE_ALLOC_STREAM              0x2
+#define SDW_STATE_CONFIG_STREAM             0x3
+#define SDW_STATE_COMPUTE_STREAM	    0x4
+#define SDW_STATE_PREPARE_STREAM            0x5
+#define SDW_STATE_ENABLE_STREAM             0x6
+#define SDW_STATE_DISABLE_STREAM            0x7
+#define SDW_STATE_UNPREPARE_STREAM          0x8
+#define SDW_STATE_UNCOMPUTE_STREAM	    0x9
+#define SDW_STATE_RELEASE_STREAM            0xa
+#define SDW_STATE_FREE_STREAM               0xb
+#define SDW_STATE_FREE_STREAM_TAG           0xc
+#define SDW_STATE_ONLY_XPORT_STREAM	    0xd
+
+#define SDW_STATE_INIT_RT		0x1
+#define SDW_STATE_CONFIG_RT		0x2
+#define SDW_STATE_PREPARE_RT		0x3
+#define SDW_STATE_ENABLE_RT		0x4
+#define SDW_STATE_DISABLE_RT		0x5
+#define SDW_STATE_UNPREPARE_RT		0x6
+#define SDW_STATE_RELEASE_RT		0x7
+
+struct sdw_runtime;
+/* Defined in sdw.c, used by multiple files of module */
+extern struct sdw_core sdw_core;
+
+enum sdw_port_state {
+	SDW_PORT_STATE_CH_READY,
+	SDW_PORT_STATE_CH_STOPPED,
+	SDW_PORT_STATE_CH_PREPARING,
+	SDW_PORT_STATE_CH_DEPREPARING,
+};
+
+enum sdw_stream_state {
+	SDW_STREAM_ALLOCATED,
+	SDW_STREAM_FREE,
+	SDW_STREAM_ACTIVE,
+	SDW_STREAM_INACTIVE,
+};
+
+enum sdw_clk_state {
+	SDW_CLK_STATE_OFF = 0,
+	SDW_CLK_STATE_ON = 1,
+};
+
+struct port_chn_en_state {
+	bool is_activate;
+	bool is_bank_sw;
+};
+
+struct sdw_stream_tag {
+	int stream_tag;
+	struct mutex stream_lock;
+	int ref_count;
+	enum sdw_stream_state stream_state;
+	char key[SDW_MAX_STREAM_TAG_KEY_SIZE];
+	struct sdw_runtime *sdw_rt;
+};
+
+struct sdw_stream_params {
+	unsigned int rate;
+	unsigned int channel_count;
+	unsigned int bps;
+};
+
+struct sdw_port_runtime {
+	int port_num;
+	enum sdw_port_state port_state;
+	int channel_mask;
+	/* Frame params and stream params are per port based
+	 * Single stream of audio may be split
+	 * into mutliple port each handling
+	 * subset of channels, channels should
+	 * be contiguous in subset
+	 */
+	struct sdw_transport_params transport_params;
+	struct sdw_port_params port_params;
+	struct list_head port_node;
+};
+
+struct sdw_slave_runtime {
+	/* Simplified port or full port, there cannot be both types of
+	 * data port for single stream, so data structure is kept per
+	 * slave runtime, not per port
+	 */
+	enum sdw_dpn_type type;
+	struct sdw_slv *slave;
+	int direction;
+	/* Stream may be split into multiple slaves, so this is for
+	 * this particular slave
+	 */
+	struct sdw_stream_params stream_params;
+	struct list_head port_rt_list;
+	struct list_head slave_sdw_node;
+	struct list_head slave_node;
+	int rt_state; /* State of runtime structure */
+
+};
+
+
+struct sdw_mstr_runtime {
+	struct sdw_master *mstr;
+	int direction;
+	/* Stream may be split between  multiple masters so this
+	 * is for invidual master, if stream is split into multiple
+	 * streams. For calculating the bandwidth on the particular bus
+	 * stream params of master is taken into account.
+	 */
+	struct sdw_stream_params stream_params;
+	struct list_head port_rt_list;
+	/* Two nodes are required because BW calculation is based on master
+	 * while stream enabling is based on stream_tag, where multiple
+	 * masters may be involved
+	 */
+	struct list_head mstr_sdw_node; /* This is to add mstr_rt in sdw_rt */
+	struct list_head mstr_node; /* This is to add mstr_rt in mstr */
+
+	struct list_head slv_rt_list;
+	 /* Individual stream bandwidth on given master */
+	unsigned int	stream_bw;
+	 /* State of runtime structure */
+	int rt_state;
+};
+
+struct sdw_runtime {
+	int tx_ref_count;
+	int rx_ref_count;
+	/* This is stream params for whole stream
+	 * but stream may be split between two
+	 * masters, or two slaves.
+	 */
+	struct sdw_stream_params stream_params;
+	struct list_head slv_rt_list;
+	struct list_head mstr_rt_list;
+	enum sdw_stream_type	type;
+	int stream_state;
+	int xport_state;
+
+};
+
+struct sdw_slv_status {
+	struct list_head node;
+	enum sdw_slave_status status[SOUNDWIRE_MAX_DEVICES+1];
+};
+
+/** Bus structure which handles bus related information */
+struct sdw_bus {
+	struct list_head bus_node;
+	struct sdw_master *mstr;
+	unsigned int	port_grp_mask[2];
+	unsigned int	slave_grp_mask[2];
+	unsigned int	clk_state;
+	unsigned int	active_bank;
+	unsigned int	clk_freq;
+	/* Bus total Bandwidth. Initialize and reset to zero */
+	unsigned int	bandwidth;
+	unsigned int	system_interval; /* Bus System Interval */
+	unsigned int	frame_freq;
+	unsigned int	col;
+	unsigned int	row;
+	struct task_struct *status_thread;
+	struct kthread_worker kworker;
+	struct kthread_work kwork;
+	struct list_head status_list;
+	spinlock_t spinlock;
+};
+
+/** Holds supported Row-Column combination related information */
+struct sdw_rowcol {
+	int     row;
+	int     col;
+	int     control_bits;
+	int data_bits;
+};
+
+/**
+ * Global soundwire structure. It handles all the streams spawned
+ * across masters and has list of bus structure per every master
+ * registered
+ */
+struct sdw_core {
+	struct sdw_stream_tag stream_tags[SDW_NUM_STREAM_TAGS];
+	struct sdw_rowcol     rowcolcomb[MAX_NUM_ROW_COLS];
+	struct list_head bus_list;
+	struct mutex core_lock;
+	struct idr idr;
+	int first_dynamic_bus_num;
+};
+
+/* Structure holding mapping of numbers to cols */
+struct sdw_num_to_col {
+	int num;
+	int col;
+};
+
+/* Structure holding mapping of numbers to rows */
+struct sdw_num_to_row {
+	int num;
+	int row;
+};
+
+int sdw_slave_port_config_port_params(struct sdw_slave_runtime *slv_rt);
+int sdw_slave_port_prepare(struct sdw_slave_runtime, bool prepare);
+int sdw_bus_bw_init(void);
+int sdw_mstr_bw_init(struct sdw_bus *sdw_bs);
+int sdw_bus_calc_bw(struct sdw_stream_tag *stream_tag, bool enable);
+int sdw_bus_calc_bw_dis(struct sdw_stream_tag *stream_tag, bool unprepare);
+int sdw_chn_enable(void);
+
+#endif /* _LINUX_SDW_PRIV_H */